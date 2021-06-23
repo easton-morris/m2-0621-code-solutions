09270547f538a14f08a12bb52d049ebead1b7d5c
easton-morris <eastonmorris9@gmail.com>
2021-06-22 21:29:56 -0700

    redefined on the constructor prototype

--- javascript-constructors/student.js
+++ javascript-constructors/student.js
@@ -1,17 +1,15 @@
 /* exported Student */
 
-const studentProto = {
-  getFullName: function () {
-    return this.firstName + ' ' + this.lastName;
-  },
-  getIntroduction: function () {
-    return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
-  }
-};
-
 function Student(firstName, lastName, subject) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.subject = subject;
-  Object.setPrototypeOf(this, studentProto);
+  Student.prototype = {
+    getFullName: function () {
+      return this.firstName + ' ' + this.lastName;
+    },
+    getIntroduction: function () {
+      return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+    }
+  };
 }
