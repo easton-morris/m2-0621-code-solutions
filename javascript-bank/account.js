97029e121d907e67f6663c23f5ad912889b93752
easton-morris <eastonmorris9@gmail.com>
2021-06-24 19:11:22 -0700

    fixed redundancies and changed proto method assignments

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -6,36 +6,32 @@ function Account(number, holder) {
   this.transactions = [];
 }
 
-Account.prototype = {
-  deposit: function (amount) {
-    let check = true;
-    if (amount > 0 && typeof amount === 'number' && Number.isInteger(amount) === true) {
-      this.transactions.push(new Transaction('deposit', amount));
-    } else {
-      check = false;
-    }
-    return check;
-  },
-  withdraw: function (amount) {
-    let check = true;
-    if (amount > 0 && typeof amount === 'number' && Number.isInteger(amount) === true) {
-      this.transactions.push(new Transaction('withdrawal', amount));
-    } else {
-      check = false;
-    }
-    return check;
-  },
-  getBalance: function () {
-    let balance = 0;
-    if (this.transactions.length > 0) {
-      for (let i = 0; i < this.transactions.length; i++) {
-        if (this.transactions[i].type === 'deposit') {
-          balance += this.transactions[i].amount;
-        } else if (this.transactions[i].type === 'withdrawal') {
-          balance -= this.transactions[i].amount;
-        }
-      }
-    }
-    return balance;
+Account.prototype.deposit = function (amount) {
+  let check = true;
+  if (amount > 0 && typeof amount === 'number' && Number.isInteger(amount)) {
+    this.transactions.push(new Transaction('deposit', amount));
+  } else {
+    check = false;
   }
+  return check;
+};
+Account.prototype.withdraw = function (amount) {
+  let check = true;
+  if (amount > 0 && typeof amount === 'number' && Number.isInteger(amount)) {
+    this.transactions.push(new Transaction('withdrawal', amount));
+  } else {
+    check = false;
+  }
+  return check;
+};
+Account.prototype.getBalance = function () {
+  let balance = 0;
+  for (let i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      balance += this.transactions[i].amount;
+    } else if (this.transactions[i].type === 'withdrawal') {
+      balance -= this.transactions[i].amount;
+    }
+  }
+  return balance;
 };
