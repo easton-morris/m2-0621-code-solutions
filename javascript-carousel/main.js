0368f6d02e688c3bf99695e7cc3a93be07273825
easton-morris <eastonmorris9@gmail.com>
2021-06-25 23:48:08 -0700

    added items, added html frame, commented out the JS plan

---
+++ javascript-carousel/main.js
@@ -0,0 +1,10 @@
+/* 1. create progress icon creating function (run on load)
+2. create image array
+3. create img switching function
+4. create setInterval call
+5. create clearInterval on progress icon click
+6. create img switch, progress icon change, and new setInterval call
+on progress icon click
+7. create img switch on arrow click
+==OPTIONAL==
+8. create JSON to pass to local storage to retain last img  */
