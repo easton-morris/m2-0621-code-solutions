9a0d91579f8d6344b920165736144f0a114bb5f0
easton-morris <eastonmorris9@gmail.com>
2021-08-05 18:57:58 -0700

    added a check for exisiting post before updating

--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const notesObj = notes.notes;
 
 function update(id, string) {
-  notesObj[id] = string;
+  if (notesObj[id] !== undefined) {
+    notesObj[id] = string;
+  }
   const notesJSON = JSON.stringify(notes, null, 2);
   fs.writeFile('data.json', notesJSON, err => {
     if (err) throw err;
