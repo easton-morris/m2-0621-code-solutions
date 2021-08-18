4495618c3d10ef017adae63a7deda3fa3033c9e7
easton-morris <eastonmorris9@gmail.com>
2021-08-17 21:15:05 -0700

    added id request and error handling for it

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 
-const notes = [];
+const notes = require('./data.json');
 
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
   const resArr = [];
-  for (const prop in notes) {
-    const resJSON = notes[prop];
+  for (const prop in notes.notes) {
+    const resJSON = notes.notes[prop];
     resArr.push(resJSON);
   }
   res.status(200).json(resArr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  if (parseInt(req.params.id) > 0) {
+    if (req.params.id in notes.notes) {
+      res.status(200).json(notes.notes[req.params.id]);
+    }
+  } else {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  }
 });
 
 // eslint-disable-next-line no-console
