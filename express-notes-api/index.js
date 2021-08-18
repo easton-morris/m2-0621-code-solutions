c630306b3e0951e75f21d0a3038d5bb3dd07ac7c
easton-morris <eastonmorris9@gmail.com>
2021-08-17 23:24:08 -0700

    added additional error handling for the id request

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -18,6 +18,8 @@ app.get('/api/notes/:id', (req, res) => {
   if (parseInt(req.params.id) > 0) {
     if (req.params.id in notes.notes) {
       res.status(200).json(notes.notes[req.params.id]);
+    } else {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
     }
   } else {
     res.status(400).json({ error: 'id must be a positive integer' });
