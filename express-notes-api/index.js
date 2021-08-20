766e9dbe101c51a0c64863a6add250b3ae43cf92
easton-morris <eastonmorris9@gmail.com>
2021-08-19 20:41:18 -0700

    finished delete op and put op with error handling

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -38,11 +38,54 @@ app.post('/api/notes', (req, res, next) => {
     };
     notes.nextId++;
   }
-  fs.writeFile('./data.json', JSON.stringify(notes), err => {
+  fs.writeFile('./data.json', JSON.stringify(notes, null, 2), err => {
     if (err) {
       res.status(500).json({ error: 'An unexpected error has occurred' });
     } else {
       res.status(201).json(notes.notes[assnID]);
+    }
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  if (parseInt(req.params.id) > 0) {
+    if (req.params.id in notes.notes) {
+      delete notes.notes[req.params.id];
+    } else {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    }
+  } else {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  }
+  fs.writeFile('./data.json', JSON.stringify(notes, null, 2), err => {
+    if (err) {
+      res.status(500).json({ error: 'An unexpected error has occurred' });
+    } else {
+      res.status(204).json();
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  if (parseInt(req.params.id) > 0 && req.body.content !== undefined) {
+    if (req.params.id in notes.notes) {
+      notes.notes[req.params.id] = {
+        id: req.params.id,
+        content: req.body.content
+      };
+    } else {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    }
+  } else if (req.body.content === undefined) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (req.body.content !== undefined) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  }
+  fs.writeFile('./data.json', JSON.stringify(notes, null, 2), err => {
+    if (err) {
+      res.status(500).json({ error: 'An unexpected error has occurred' });
+    } else {
+      res.status(200).json(notes.notes[req.params.id]);
     }
   });
 });
