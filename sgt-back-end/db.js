84f61fce69e9cb5d2ea68847851d89d53b9cc00f
easton-morris <eastonmorris9@gmail.com>
2021-08-31 19:48:04 -0700

    sgt-back-end part1

---
+++ sgt-back-end/db.js
@@ -0,0 +1,11 @@
+const pg = require('pg');
+
+// only create ONE pool for your whole server
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+exports.db = db;
